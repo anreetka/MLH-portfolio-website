1c949775039db32ea2065248ab716a1d3ad7ca7e
Anreet Kaur <anreetka@sheridancollege.ca>
2024-07-14 16:03:15 -0400

    restructured html pages and added api endpoints for creating and deleting post

--- app/__init__.py
+++ app/__init__.py
@@ -32,27 +32,20 @@ name="Anreet Kaur"
 
 @app.route('/')
 def index():
-    active_page = 'home'
-    return render_template('index.html', name="Anreet Kaur", active_page=active_page, url=os.getenv("URL"))
-
-@app.route('/work_experiences')
-def work_experiences():
-    work_experiences=[
-        {'company': 'MLH Fellowship', 'position': 'Production Engineering Fellow', 'duration': 'Jun 2024 - Present', 'description': 'Received acceptance into highly selective (acceptance rate < 2.5%) Production Engineering program sponsored by Meta through MLH Fellowship'},
-        {'company':'SIRT Centre - Research', 'position': 'Junior Programmer Intern', "duration": 'Jan 2024 - Apr 2024', 'description':'Got nominated for Generator Student Awards 2024 in "Team Collaboration in Research" category for outstanding teamwork in the Virtual Reality project during my internship'},
-        {'company':'SkillHat', 'position': 'Diversity in Tech Talent: Web Development Fellow', "duration": 'Jan 2024 - Mar 2024', 'description':'Selected from over 1,000+ applicants for a $10,000 scholarship for this 3-month tech fellowship on  web development with best practices. Additionally, developed push notification features for a community project with JavaScript and Web Push Library, allowing smoother integration with Express js backend'},
-        {'company':'Sheridan College', 'position': 'Peer Mentor - Team Lead', "duration": 'Sep 2023 - Dec 2023', 'description':'Earned nomination for Student Choice – Dare to Care Leadership Award for 2023-2024, recognizing exceptional commitment of a student leader to a supportive and inclusive campus environment'}
-    ]
-    return render_template('work_experiences.html', name=name, title="Work Experiences", work_experiences=work_experiences)
-
-@app.route('/education')
-def education():
 
     education=[
-        {'institution': 'Sheridan College', 'degree': 'Computer Science Co-op (3-year)', 'duration': 'Dec 2025'}
+            {'institution': 'Sheridan College', 'degree': 'Associate\'s degree - Software Eng. Co-op', 'duration': '2022-2025', 'image':'https://www.transitionresourceguide.ca/sites/default/files/styles/hero_image/public/img/hero/Sheridan_1.png?itok=ZdEp8P3X'}
     ]
 
-    return render_template('education.html', name=name, title="Education", education=education)
+    work_experiences=[
+            {'company': 'Royal Bank of Canada - RBC', 'position': 'Incoming Software Engineering Intern', 'duration': 'Fall 2024', 'description': 'Product and Services - Technology & Operations Department'},
+            {'company': 'MLH Fellowship', 'position': 'Meta Production Engineering Fellow', 'duration': 'Jun 2024 - Present', 'description': 'Received acceptance into highly selective (acceptance rate < 2.5%) Production Engineering program sponsored by Meta'},
+            {'company':'SIRT Centre - Research', 'position': 'Junior Programmer Intern', "duration": 'Jan 2024 - Apr 2024', 'description':'VR / Game Development using Unreal Engine 5 and C++. Got nominated for Generator Student Awards 2024 in "Team Collaboration in Research" category for outstanding teamwork in the Virtual Reality project during my internship'},
+            {'company':'SkillHat', 'position': 'DTTP Web Development Fellow', "duration": 'Jan 2024 - Mar 2024', 'description':'Selected from over 1,000+ applicants for a $10,000 scholarship for this 3-month tech fellowship. Developed push notification features for a community project with JavaScript and Web Push Library, allowing smoother integration with Express js backend'},
+            {'company':'Sheridan College', 'position': 'Peer Mentor - Team Lead', "duration": 'Sep 2023 - Dec 2023', 'description':'Earned nomination for 2023 Student Choice – Dare to Care Leadership Award, recognizing exceptional commitment of a student leader to a supportive and inclusive campus environment'}
+    ]
+
+    return render_template('index.html', name="Anreet Kaur", education=education, active_page="home", work_experiences=work_experiences, url=os.getenv("URL"))
 
 @app.route('/hobbies')
 def hobbies():
@@ -118,4 +111,13 @@ def delete_time_line_post():
     
     except DoesNotExist:
         return f"\nA post from {name} at {email} was not found in the database. please check again!"
+    
 
+@app.route('/timeline')
+def timeline():
+    post_data=[
+            model_to_dict(p)
+            for p in
+                TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    return render_template('timeline.html', title="Timeline", post_data=post_data)
