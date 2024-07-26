972e16f48d644405439754e690a750341158967e
Anreet Kaur <anreetka@sheridancollege.ca>
2024-07-26 16:52:31 -0400

    added changes to redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,10 +6,8 @@ cd ~/MLH-portfolio-website
 
 git fetch && git reset origin/main --hard || exit 1
 
-docker compose -f docker-compose.prod.yml down || exit 1
+docker-compose -f docker-compose.prod.yml down || exit 1
 
-docker compose -f docker-compose.prod.yml up -d --build || exit 1
+docker-compose -f docker-compose.prod.yml up -d --build || exit 1
 
-echo "Site has successfully been redeployed! :)"
-
-docker compose -f docker-compose.prod.yml ps
+docker-compose -f docker-compose.prod.yml ps
