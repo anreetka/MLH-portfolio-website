ebb0faea531bb7aaa2e0b37c51151a6e68a9c930
Anreet Kaur <anreetka@sheridancollege.ca>
2024-07-26 16:31:40 -0400

    updated script to deploy from running docker container

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,14 +6,8 @@ cd ~/MLH-portfolio-website
 
 git fetch && git reset origin/main --hard
 
-python -m venv python3-virtualenv
+docker compose -f docker-compose.prod.yml down
 
-source python3-virtualenv/bin/activate
-
-pip install -r requirements.txt
-
-systemctl daemon-reload
-
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml up -d --build
 
 echo "Site has successfully been redeployed! :)"
