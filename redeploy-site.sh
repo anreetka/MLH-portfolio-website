730a286cfcd12bebc1938c87c47ecf26a54e4e9a
Anreet Kaur <anreetka@sheridancollege.ca>
2024-07-26 17:01:00 -0400

    changing redeployment script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -4,11 +4,11 @@ pkill -f tmux
 
 cd ~/MLH-portfolio-website
 
-git fetch && git reset origin/main --hard || exit 1
+git fetch && git reset origin/main --hard 
 
-docker compose -f docker-compose.prod.yml down || exit 1
+docker compose -f docker-compose.prod.yml down
 
-docker compose -f docker-compose.prod.yml up -d --build || exit 1
+docker compose -f docker-compose.prod.yml up -d --build
 
 echo "Site has been deployed successfully! :)"
 
