9f97fff7a918a7fd5197e9403a67a2526e0593a0
Anreet Kaur <anreetka@sheridancollege.ca>
2024-07-26 16:55:10 -0400

    troubleshooting redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,8 +6,11 @@ cd ~/MLH-portfolio-website
 
 git fetch && git reset origin/main --hard || exit 1
 
-docker-compose -f docker-compose.prod.yml down || exit 1
+docker compose -f docker-compose.prod.yml down || exit 1
 
-docker-compose -f docker-compose.prod.yml up -d --build || exit 1
+docker compose -f docker-compose.prod.yml up -d --build || exit 1
 
-docker-compose -f docker-compose.prod.yml ps
+echo "Site has been deployed successfully! :)"
+
+
+
